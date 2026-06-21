09bcc085f956576f0dc4f835891f6244e14ee27c
Arsya <arsyanandaa10@gmail.com>
2026-06-21 15:06:06 +0700

    Initial: Setup Spring Boot Project

---
+++ src/test/java/com/takumagamestore/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takumagamestore.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7fae95dd2462b948acdf956a000c9d9085b33917
Arsya <arsyanandaa10@gmail.com>
2026-06-28 13:33:07 +0700

    feat: add cors and setup endpoint auth google

--- src/main/java/com/takumagamestore/demo/controller/AuthController.java
+++ src/main/java/com/takumagamestore/demo/controller/AuthController.java
@@ -1,12 +1,18 @@
 package com.takumagamestore.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.takumagamestore.demo.dto.AuthDTO;
 import com.takumagamestore.demo.model.User;
 import com.takumagamestore.demo.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
